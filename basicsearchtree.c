875ca9d58540daaacec5a69072482e21642bbdc3
Aneesh Gupta <agupta27@ncsu.edu>
2017-02-09 19:45:55 -0500

    basic functions

--- basicsearchtree.c
+++ basicsearchtree.c
@@ -8,7 +8,7 @@ typedef struct tr_n_t {key_t      key;
                 struct tr_n_t  *right;
                /* possibly additional information */ } tree_node_t;
 
-
+typedef struct tree_node_t * text_t;
 #define BLOCKSIZE 256
 
 tree_node_t *currentblock = NULL;
@@ -312,3 +312,46 @@ int main()
    return(0);
 }
 
+// the main functions
+
+text_t * create_text() {
+  /* creates an empty text, whose length is 0. */
+  return NULL;
+}
+int length_text( text_t *txt) {
+  /* returns the number of lines of the current text. */
+  return 0;
+}
+
+char * get_line( text_t *txt, int index) {
+  /* gets the line of number index, if such a line exists, and
+    returns NULL else.
+  */
+  return NULL;
+}
+
+void append_line( text_t *txt, char * new_line) {
+  /* appends new line as new last line. */
+}
+
+char * set_line( text_t *txt, int index, char * new_line) {
+  /* sets the line of number index, if such
+    a line exists, to new line, and returns a pointer to the previous line of that number. If no line of
+    that number exists, it does not change the structure and returns NULL.
+  */
+  return NULL;
+}
+
+void insert_line( text_t *txt, int index, char * new_line) {
+  /* inserts the line before the line of
+     number index, if such a line exists, to new line, renumbering all lines after that line. If no such
+    line exists, it appends new line as new last line.
+  */
+}
+
+char * delete_line( text_t *txt, int index) {
+  /* deletes the line of number index, renumbering all
+    lines after that line, and returns a pointer to the deleted line.
+  */
+  return NULL;
+}
